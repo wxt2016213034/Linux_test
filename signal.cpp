af8236fd607deae2c59778d38b2a6e89d8d77588
王旭韬 <wangxutao@wangxutaodeMacBook-Pro.local>
2022-01-20 12:26:10 +0000

    feat: signal

--- signal.cpp
+++ signal.cpp
@@ -1,16 +1,16 @@
 #include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
-int interrupt() {
+void interrupt(int a) {
 printf("Interrupt called\n"); 
 sleep(3);
 printf("Interrupt Func Ended.\n");
-}
-int catchquit() {
+};
+void catchquit(int b) {
 printf("Quit called\n"); 
 sleep(3);
 printf("Quit ended.\n");
-} 
+} ;
 int main() {
 signal(SIGINT,interrupt); signal(SIGQUIT,catchquit); printf("Interrupt set for SIGINT\n"); sleep(10);
 printf("Program NORMAL ended.\n");
